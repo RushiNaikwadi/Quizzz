46670e7d2f304881b824e31483293a4341496077
Rushikesh Naikwadi <i.rushi.naikwadi@gmail.com>
2022-12-01 17:10:55 +0530

    learned about buttons, visible/non-visible widgets, stateful vs stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
+class QuizApp extends StatefulWidget {
+  @override
+  State <StatefulWidget> createState() {
+    return QuizAppState();
+  }
+}
+
+class QuizAppState extends State <QuizApp> {
+  int mcqIndex = 0;
+
+  var mcqList = [
+    'Which is the smallest state in India ?',
+    'What is the capital of Maharashtra ?',
+    'Which is a national animal of India ?'
+  ];
+
+  void optionSelected(int optionNumber) {
+    setState(() {
+      mcqIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +31,30 @@ class QuizApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('This is quiz app'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(mcqList[mcqIndex]),
+              ElevatedButton(
+                child: Text('Option 1'),
+                onPressed: () => optionSelected(1)
+              ),
+              ElevatedButton(
+                child: Text('Option 2'),
+                  onPressed: () => optionSelected(2)
+              ),
+              ElevatedButton(
+                child: Text('Option 3'),
+                  onPressed: () => optionSelected(3)
+              ),
+              ElevatedButton(
+                child: Text('Option 4'),
+                onPressed: () => optionSelected(4)
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
